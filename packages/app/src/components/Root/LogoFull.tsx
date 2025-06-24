8b0e28562b445be778cb42fd92ec5bc4371f1ad5
Julio Cesar <cesar01_99@hotmail.com>
2025-06-23 22:13:50 -0700

    se agregaron diseños

--- packages/app/src/components/Root/LogoFull.tsx
+++ packages/app/src/components/Root/LogoFull.tsx
@@ -1,5 +1,5 @@
 /*import { makeStyles } from '@material-ui/core';*/
-import CustomLogoFull from './superset-logo-horiz.png';
+import CustomLogoFull from './backstage-logo.png';
 
 const LogoFull = () => {
   return (
